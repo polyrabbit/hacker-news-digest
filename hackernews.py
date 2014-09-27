6bc9392867f459c0239666050dbed54de88b2cd8
poly <mcx_221@foxmail.com>
2014-09-28 02:12:12 +0800

    A convenient way to drop db

--- hackernews.py
+++ hackernews.py
@@ -16,7 +16,7 @@ opener = urllib2.build_opener(cookie_support)
 urllib2.install_opener(opener)
 
 class HackerNews(object):
-    end_point = 'https://news.ycombinator.com/'
+    end_point = 'https://news.ycombinator.com/?p=5'
     storage_class = HnStorage
 
     def __init__(self):
