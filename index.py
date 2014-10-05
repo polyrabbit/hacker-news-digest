9ab3806b08bc1e20030839e4f9344db6cbd02508
poly <mcx_221@foxmail.com>
2014-10-05 15:03:13 +0800

    Add last synced

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 import logging
+from datetime import datetime
 
 from flask import (
     Flask, render_template, abort, request, send_from_directory, send_file,
     Response
 )
+from ago import human
 
 import config
 from db import ImageStorage
@@ -13,37 +15,45 @@ from startupnews import StartupNews
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+# [0] for hackernews, [1] for startupnews
+last_synced = [None, None]
 
 # @app.before_request
 # def before_request():
-#     print '+'*10, request.path
-#
+#     print last_synced
+
 @app.route("/hackernews")
 @app.route('/')
 def hackernews():
     hn = HackerNews()
-    return render_template('index.html', title='Hacker News',
-            news_list=hn.get_all(), navs=[
-            ('Hacker News', 'https://news.ycombinator.com/news'),
-            ('New', 'https://news.ycombinator.com/newest'),
-            ('Comments', 'https://news.ycombinator.com/newcomments'),
-            ('Show', 'https://news.ycombinator.com/show'),
-            ('Ask', 'https://news.ycombinator.com/ask'),
-            ('Jobs', 'https://news.ycombinator.com/jobs'),
-            ('Submit', 'https://news.ycombinator.com/submit')]
+    return render_template('index.html',
+            title='Hacker News',
+            news_list=hn.get_all(),
+            navs=[
+                ('Hacker News', 'https://news.ycombinator.com/news'),
+                ('New', 'https://news.ycombinator.com/newest'),
+                ('Comments', 'https://news.ycombinator.com/newcomments'),
+                ('Show', 'https://news.ycombinator.com/show'),
+                ('Ask', 'https://news.ycombinator.com/ask'),
+                ('Jobs', 'https://news.ycombinator.com/jobs'),
+                ('Submit', 'https://news.ycombinator.com/submit')],
+            last_synced = last_synced[0] and human(last_synced[0], 1)
         )
 
 @app.route("/startupnews")
 def startupnews():
     sn = StartupNews()
-    return render_template('index.html', title='Startup News',
-            news_list=sn.get_all(), navs=[
-            ('Startup News', 'http://news.dbanotes.net/news'),
-            ('New', 'http://news.dbanotes.net/newest'),
-            ('Comments', 'http://news.dbanotes.net/newcomments'),
-            ('Leaders', 'http://news.dbanotes.net/leaders'),
-            ('Submit', 'http://news.dbanotes.net/submit')
-        ])
+    return render_template('index.html',
+            title='Startup News',
+            news_list=sn.get_all(),
+            navs=[
+                ('Startup News', 'http://news.dbanotes.net/news'),
+                ('New', 'http://news.dbanotes.net/newest'),
+                ('Comments', 'http://news.dbanotes.net/newcomments'),
+                ('Leaders', 'http://news.dbanotes.net/leaders'),
+                ('Submit', 'http://news.dbanotes.net/submit')],
+            last_synced = last_synced[1] and human(last_synced[1], 1)
+        )
 
 @app.route('/img/<int:img_id>')
 def image(img_id):
@@ -62,9 +72,10 @@ def update(what=None):
         abort(401)
     if what == 'hackernews' or what is None:
         HackerNews().update()
-        # TODO add last-updated
+        last_synced[0] = datetime.now()
     if what == 'startupnews' or what is None:
         StartupNews().update()
+        last_synced[1] = datetime.now()
     return 'Great success!'
 
 @app.route('/sitemap.xml')
