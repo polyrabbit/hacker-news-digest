3affaa83f1959b0020d4556d2e6e661465bede9f
Changxin Miao <miaochangxin@step.ai>
2025-04-01 10:26:53 +0800

    Avoid checking whole list everytime to reduce transfer cost to DB

--- db/image.py
+++ db/image.py
@@ -1,6 +1,7 @@
 # TODO: need a separate model?
 import logging
 import os
+import random
 import time
 
 from sqlalchemy import String, column, Values, select
@@ -21,7 +22,10 @@ WHERE summary.image_name IS NULL
 def expire():
     start = time.time()
     removed = 0
-    for img_files in chunks(os.listdir(config.image_dir), 500):
+    all_files = list(os.listdir(config.image_dir))
+    random.shuffle(all_files) # avoid checking whole list everytime to reduce transfer cost to DB
+    candidates = all_files[:1000]
+    for img_files in chunks(candidates, 500):
         values = Values(column('name', String), name='v').data(list(map(lambda x: (x,), img_files)))
         stmt = select(values).join(Summary, Summary.image_name == values.c.name,
                                    isouter=True  # Add this to implement left outer join
@@ -32,7 +36,7 @@ def expire():
                 os.remove(os.path.join(config.image_dir, image_name[0]))
                 removed += 1
     cost = (time.time() - start) * 1000
-    logger.info(f'removed {removed} feature images, cost(ms): {cost:.2f}')
+    logger.info(f'removed {removed}/{len(candidates)} feature images, cost(ms): {cost:.2f}')
 
 
 def chunks(lst, n):
